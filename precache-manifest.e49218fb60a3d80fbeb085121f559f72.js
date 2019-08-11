cb5788a7ce7f6990460775b811588f3d2d648b42
Roman <kotenkoroman2019@gmail.com>
2019-08-11 13:52:45 +0300

    Updates

---
+++ precache-manifest.e49218fb60a3d80fbeb085121f559f72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "301b49a0eabc96d5e0a043393caa6a15",
+    "url": "/react_burger/index.html"
+  },
+  {
+    "revision": "2b85e7a30e37fc07ed63",
+    "url": "/react_burger/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "523b496352061651725d",
+    "url": "/react_burger/static/js/2.fd35ecd8.chunk.js"
+  },
+  {
+    "revision": "2b85e7a30e37fc07ed63",
+    "url": "/react_burger/static/js/main.664bbabe.chunk.js"
+  },
+  {
+    "revision": "8f6f67cdbde442a1acd7",
+    "url": "/react_burger/static/js/runtime~main.44df9e8f.js"
+  }
+]);
