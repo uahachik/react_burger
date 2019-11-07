1f35be02a77763452f7220bbc65d7d4e145f0707
Roman <kotenkoroman2019@gmail.com>
2019-11-07 09:47:42 +0200

    deploy heroku

--- server.js
+++ server.js
@@ -2,9 +2,22 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const PORT = process.env.PORT || 5000;
 
 server.use(middlewares);
 server.use(router);
+
+// Serve static assets in prodaction
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder // set that index.html inside client/build to be a static file
+  // so we need set build folder as a folder
+  server.use(express.static('client/build'));
+
+  // serve index.html
+  server.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
